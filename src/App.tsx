0d19586926792a295f5b33def4c478878b534527
seitamuro <seitamuro0210@gmail.com>
2023-10-28 16:35:44 +0900

    キャンバスが2つ生成される問題を解決

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Sketch from "react-p5";
 import p5Types from "p5";
 
 import { Agent } from './Agent';
+import { useRef } from 'react';
 
 function App() {
-
   const agents: Agent[] = []
-  const agent_num = 10;
+  const agent_num = 20;
+  const initialized = useRef(false);
 
   const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    if (!initialized.current) {
+      p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+      initialized.current = true;
+    }
     for (let i = 0; i < agent_num; i++) {
       agents.push(new Agent(p5));
     }
@@ -29,7 +33,9 @@ function App() {
   const windowResized = (p5: p5Types) => {
   }
 
-  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
+  return (
+    <Sketch setup={setup} draw={draw} windowResized={windowResized} />
+  )
 }
 
 export default App;
